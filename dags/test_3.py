8c18b631b8c37c3005893a4133944573a9bbd3b6
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2023-07-03 11:27:42 +0700

    Update test_3.py

--- dags/test_3.py
+++ dags/test_3.py
@@ -3,6 +3,22 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import date, datetime, timedelta
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from shapely import geometry
+from sqlalchemy import create_engine
+from shapely.geometry.multipolygon import MultiPolygon
+import re
+from shapely import wkt
+import geopandas as gpd
+import pandas as pd
+import json
+import requests
+import numpy as np
+import xmltodict
+import requests_cache
+import shapely as shp
+import geowrangler
+from geowrangler import grids
 
 ## Arguments applied to the tasks, not the DAG in itself 
 default_args={
